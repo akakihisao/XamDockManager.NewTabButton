52bf47e0e418e8d3616e02c081e3b3b17b2683c2
Jason Tyler <jasonjtyler@gmail.com>
2012-10-07 23:14:52 -0700

    Inital commit

---
+++ DockManagerCommands.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace XamDockManager.NewTabButton
+{
+    class DockManagerCommands
+    {
+        private static RoutedUICommand _newTab;
+
+        public static RoutedUICommand NewTab { get { return _newTab; } }
+
+        static DockManagerCommands()
+        {
+            _newTab = new RoutedUICommand("New Tab", "NewTab", typeof(DockManagerCommands));
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Infragistics.Windows.DockManager;
+
+namespace XamDockManager.NewTabButton
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (Object.ReferenceEquals(e.Command, DockManagerCommands.NewTab))
+            {
+                var newPane = new ContentPane();
+                newPane.Header = "New Tab";
+                this.tabsPane.Items.Add(newPane);
+            }
+        }
+    }
+}
